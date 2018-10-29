7e01016920583563083c7eaf0e2da60c15206546
Henrique Aguilar <henriaguilar@gmail.com>
2018-10-29 13:34:52 -0300

    all tasks done, missing CSS

--- app/controllers/questions_controller_controller.rb
+++ app/controllers/questions_controller_controller.rb
@@ -1,5 +1,14 @@
 class QuestionsControllerController < ApplicationController
   def answer
-    @answer = params[:question]
+    unless params[:question].nil?
+      @question = params[:question].downcase
+      if @question == "i am going to work"
+        @answer = "Great"
+      elsif @question.last == "?"
+       @answer = "Silly question, get dressed and go to work!"
+      else
+        @answer = "I don't care, get dressed and go to work!"
+      end
+    end
   end
 end
